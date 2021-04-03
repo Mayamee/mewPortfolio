9fe87f4a55773beaa05238f46f33ac12cfa6d1ae
Dmitry <ponyashcat228@gmail.com>
2021-04-03 12:22:44 +0300

    подчистил комментарии

--- app/js/user/_DR.js
+++ app/js/user/_DR.js
@@ -66,28 +66,6 @@ $(document).ready(function () {
     $("*.slick-slide").removeClass("center-slide");
     $(".slick-current").next().addClass("center-slide");
   });
-  //   //   простой фильтр
-  //   let filter = $("[data-filter");
-  //   filter.on("click", function (event) {
-  //     event.preventDefault();
-  //     let cat = $(this).data("filter");
-  //     $("[data-cat]").each(function () {
-  //       let item = $(this).data("cat");
-  //       if (cat == "all") {
-  //         $(this).removeClass("hide");
-  //       } else {
-  //         if (item != cat) {
-  //           $(this).addClass("hide");
-  //           setTimeout(function () {
-  //             $(this).addClass("hide");
-  //           }, 1000);
-  //         } else {
-  //           $(this).removeClass("hide");
-  //         }
-  //       }
-  //     });
-  //   });
-  //   //   простой фильтр
   const container = document.querySelector(".gallery__columns");
   const gallerySizer = document.querySelector(".gallery__sizer");
   const shuffleGrid = new Shuffle(container, {
